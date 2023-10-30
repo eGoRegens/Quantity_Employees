d383cdfbacb4aad41b5e3ba77708921b9d6b3d12
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 22:51:56 +0400

    назначение констант

--- src/main/java/NumberOfEmployees.java
+++ src/main/java/NumberOfEmployees.java
@@ -1,14 +1,14 @@
 public class NumberOfEmployees {
     public static void main(String[] args) {
-        int quantityProgrammers = 3;
-        int quantityTestersForProgrammers = 1; // кол-во тестеров на одного программиста
-        int quantitySupports = 2; // кол-во поддержки на обработку жалоб по поводу кода 1 программиста.
-        int totalTesters = quantityProgrammers * quantityTestersForProgrammers;
-        int totalSupports = quantityProgrammers * quantitySupports;
-        int totalEmployees = quantityProgrammers + totalTesters + totalSupports;
+        final int QUANTITY_PROGRAMMERS = 3;
+        final int RATIO_TESTERS = 1; // кол-во тестеров на одного программиста
+        final int RATIO_SUPPORTS = 2; // кол-во поддержки на обработку жалоб по поводу кода 1 программиста.
+        final int TOTAL_TESTERS = QUANTITY_PROGRAMMERS * RATIO_TESTERS;
+        final int TOTAL_SUPPORTS = QUANTITY_PROGRAMMERS * RATIO_SUPPORTS;
+        final int TOTAL_EMPLOYEES = QUANTITY_PROGRAMMERS + TOTAL_TESTERS + TOTAL_SUPPORTS;
 
-        System.out.println("Требуемое кол-во тестировщиков на проекта = " + totalTesters);
-        System.out.println("Требуемое кол-во специалистов поддержки = " + totalSupports);
-        System.out.println("Общее кол-во сотрудников требуемое на проекте = " + totalEmployees);
+        System.out.println("Требуемое кол-во тестировщиков на проекта = " + TOTAL_TESTERS);
+        System.out.println("Требуемое кол-во специалистов поддержки = " + TOTAL_SUPPORTS);
+        System.out.println("Общее кол-во сотрудников требуемое на проекте = " + TOTAL_EMPLOYEES);
     }
 }
